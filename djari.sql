5fd7d10901fc56f29b056068aa9ec8ad0036acf2
khisan <khisan8@gmail.com>
2020-05-22 07:59:32 +0700

    upload db baru

--- djari.sql
+++ djari.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 19 Feb 2020 pada 06.33
+-- Waktu pembuatan: 22 Bulan Mei 2020 pada 02.58
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.2
 
@@ -25,6 +25,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `tb_client`
+--
+
+CREATE TABLE `tb_client` (
+  `id_client` int(11) NOT NULL,
+  `nama_client` varchar(500) NOT NULL,
+  `alamat` varchar(500) NOT NULL,
+  `id_product` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_client`
+--
+
+INSERT INTO `tb_client` (`id_client`, `nama_client`, `alamat`, `id_product`) VALUES
+(7, 'Bokur', 'Jl Dr Suhardjo No 30 A-B Depan Muktamar/Lap, pondok, Lirboyo, Kec. Mojoroto, Kediri, Jawa Timur 64116, Indonesia', 1),
+(9, 'Hamdan', 'Jl. Jenderal Basuki Rahmat No.32, Kauman, Kec. Klojen, Kota Malang, Jawa Timur 65119, Indonesia', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `tb_dokumentasi`
 --
 
@@ -41,7 +62,8 @@ CREATE TABLE `tb_dokumentasi` (
 
 INSERT INTO `tb_dokumentasi` (`id_dokumentasi`, `id_kategori`, `gambar`, `keterangan`) VALUES
 (4, 1, '480b6edd535f2f2fc09449cf0df2dd52.jpg', 'Wedding cuy'),
-(5, 2, '32c2c7f579c74394794bd6d2bb906435.jpg', 'Family Shoot');
+(5, 2, '32c2c7f579c74394794bd6d2bb906435.jpg', 'Family Shoot'),
+(6, 1, '4a7d3b52de0839bd06e8e44dc52ba6d9.jpg', 'Wedding 2');
 
 -- --------------------------------------------------------
 
@@ -51,10 +73,18 @@ INSERT INTO `tb_dokumentasi` (`id_dokumentasi`, `id_kategori`, `gambar`, `ketera
 
 CREATE TABLE `tb_jadwal` (
   `id_jadwal` int(11) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `alamat` varchar(100) NOT NULL,
+  `id_client` int(11) NOT NULL,
+  `alamat` text NOT NULL,
   `tanggal` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_jadwal`
+--
+
+INSERT INTO `tb_jadwal` (`id_jadwal`, `id_client`, `alamat`, `tanggal`) VALUES
+(9, 7, 'Jl Dr Suhardjo No 30 A-B Depan Muktamar/Lap, pondok, Lirboyo, Kec. Mojoroto, Kediri, Jawa Timur 64116, Indonesia', '2020-05-26'),
+(10, 9, 'Jl. Jenderal Basuki Rahmat No.32, Kauman, Kec. Klojen, Kota Malang, Jawa Timur 65119, Indonesia', '2020-05-31');
 
 -- --------------------------------------------------------
 
@@ -65,8 +95,7 @@ CREATE TABLE `tb_jadwal` (
 CREATE TABLE `tb_jadwal_selesai` (
   `id_jadwal_selesai` int(11) NOT NULL,
   `id_jadwal` int(11) NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `alamat` varchar(100) NOT NULL,
+  `id_client` int(11) NOT NULL,
   `tanggal` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -74,9 +103,8 @@ CREATE TABLE `tb_jadwal_selesai` (
 -- Dumping data untuk tabel `tb_jadwal_selesai`
 --
 
-INSERT INTO `tb_jadwal_selesai` (`id_jadwal_selesai`, `id_jadwal`, `nama`, `alamat`, `tanggal`) VALUES
-(1, 1, 'Dikaso', 'jl.sidoasri', '2020-01-16'),
-(2, 2, 'cobak', 'Jl.mawar', '2020-01-17');
+INSERT INTO `tb_jadwal_selesai` (`id_jadwal_selesai`, `id_jadwal`, `id_client`, `tanggal`) VALUES
+(8, 9, 7, '2020-05-26');
 
 -- --------------------------------------------------------
 
@@ -100,6 +128,46 @@ INSERT INTO `tb_kategori` (`id_kategori`, `nama_kategori`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `tb_pembayaran`
+--
+
+CREATE TABLE `tb_pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_client` int(11) NOT NULL,
+  `nominal` int(11) NOT NULL,
+  `keterangan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_pembayaran`
+--
+
+INSERT INTO `tb_pembayaran` (`id_pembayaran`, `id_client`, `nominal`, `keterangan`) VALUES
+(6, 7, 30000, 'Lunas');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_product`
+--
+
+CREATE TABLE `tb_product` (
+  `id_product` int(11) NOT NULL,
+  `nama_product` varchar(500) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `keterangan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_product`
+--
+
+INSERT INTO `tb_product` (`id_product`, `nama_product`, `harga`, `keterangan`) VALUES
+(1, 'Canon EOS 7D Kit l', 870000, 'test cuy');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `tb_user`
 --
 
@@ -117,38 +185,61 @@ CREATE TABLE `tb_user` (
 --
 
 INSERT INTO `tb_user` (`id_user`, `nama`, `jabatan`, `username`, `password`, `gambar`) VALUES
-(1, 'Walter White cuy', 'Chief Executive Officer cuy', 'ceo cuy', '81dc9bdb52d04dc20036dbd8313ed055', 'team-1.jpg'),
-(2, 'Sarah Jhonson', 'Product Manager', 'pm', '81dc9bdb52d04dc20036dbd8313ed055', 'team-2.jpg'),
-(3, 'William Anderson', 'Chief Technology Officer', 'cto', '81dc9bdb52d04dc20036dbd8313ed055', 'team-3.jpg'),
-(4, 'Amanda Jepson', 'Accountant', 'act', '81dc9bdb52d04dc20036dbd8313ed055', 'team-4.jpg');
+(1, 'Walter White ', 'Chief Executive Officer ', 'ceo ', '81dc9bdb52d04dc20036dbd8313ed055', 'team-1.jpg'),
+(2, 'Sarah Jhonson', 'Vice Chief Executive Officer', 'vc', '81dc9bdb52d04dc20036dbd8313ed055', 'team-2.jpg'),
+(3, 'William Anderson ', 'Employee ', 'em ', '81dc9bdb52d04dc20036dbd8313ed055', 'testimonial-4.jpg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indeks untuk tabel `tb_client`
+--
+ALTER TABLE `tb_client`
+  ADD PRIMARY KEY (`id_client`),
+  ADD KEY `tb_client_ibfk_1` (`id_product`);
+
+--
 -- Indeks untuk tabel `tb_dokumentasi`
 --
 ALTER TABLE `tb_dokumentasi`
-  ADD PRIMARY KEY (`id_dokumentasi`);
+  ADD PRIMARY KEY (`id_dokumentasi`),
+  ADD KEY `id_kategori` (`id_kategori`);
 
 --
 -- Indeks untuk tabel `tb_jadwal`
 --
 ALTER TABLE `tb_jadwal`
-  ADD PRIMARY KEY (`id_jadwal`);
+  ADD PRIMARY KEY (`id_jadwal`),
+  ADD KEY `tb_jadwal_ibfk_1` (`id_client`);
 
 --
 -- Indeks untuk tabel `tb_jadwal_selesai`
 --
 ALTER TABLE `tb_jadwal_selesai`
-  ADD PRIMARY KEY (`id_jadwal_selesai`);
+  ADD PRIMARY KEY (`id_jadwal_selesai`),
+  ADD KEY `tb_jadwal_selesai_ibfk_1` (`id_jadwal`),
+  ADD KEY `tb_jadwal_selesai_ibfk_2` (`id_client`);
 
 --
 -- Indeks untuk tabel `tb_kategori`
 --
 ALTER TABLE `tb_kategori`
   ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `tb_pembayaran`
+--
+ALTER TABLE `tb_pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`),
+  ADD KEY `id_client` (`id_client`);
+
+--
+-- Indeks untuk tabel `tb_product`
+--
+ALTER TABLE `tb_product`
+  ADD PRIMARY KEY (`id_product`);
 
 --
 -- Indeks untuk tabel `tb_user`
@@ -161,22 +252,28 @@ ALTER TABLE `tb_user`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `tb_client`
+--
+ALTER TABLE `tb_client`
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT untuk tabel `tb_dokumentasi`
 --
 ALTER TABLE `tb_dokumentasi`
-  MODIFY `id_dokumentasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_dokumentasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_jadwal`
 --
 ALTER TABLE `tb_jadwal`
-  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_jadwal_selesai`
 --
 ALTER TABLE `tb_jadwal_selesai`
-  MODIFY `id_jadwal_selesai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_jadwal_selesai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_kategori`
@@ -185,10 +282,56 @@ ALTER TABLE `tb_kategori`
   MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT untuk tabel `tb_pembayaran`
+--
+ALTER TABLE `tb_pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_product`
+--
+ALTER TABLE `tb_product`
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT untuk tabel `tb_user`
 --
 ALTER TABLE `tb_user`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tb_client`
+--
+ALTER TABLE `tb_client`
+  ADD CONSTRAINT `tb_client_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `tb_product` (`id_product`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tb_dokumentasi`
+--
+ALTER TABLE `tb_dokumentasi`
+  ADD CONSTRAINT `tb_dokumentasi_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `tb_kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tb_jadwal`
+--
+ALTER TABLE `tb_jadwal`
+  ADD CONSTRAINT `tb_jadwal_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `tb_client` (`id_client`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ketidakleluasaan untuk tabel `tb_jadwal_selesai`
+--
+ALTER TABLE `tb_jadwal_selesai`
+  ADD CONSTRAINT `tb_jadwal_selesai_ibfk_1` FOREIGN KEY (`id_jadwal`) REFERENCES `tb_jadwal` (`id_jadwal`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tb_pembayaran`
+--
+ALTER TABLE `tb_pembayaran`
+  ADD CONSTRAINT `tb_pembayaran_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `tb_client` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
